6c2917552603a77bac34a97b9bc2d5c566ee8b88
fiture <fiture.cn@gmail.com>
2014-07-01 21:32:56 +0800

    update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,9 @@
 
     <footer>
         <div class="wrapper">
-        &copy; <?php echo date('Y') ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> (fiture.me)
+            &copy; <?php echo date('Y') ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> (fiture.me) 
+            / Theme by <a href="http://fiture.me">fiture</a> 
+            / Host by <a href="https://www.digitalocean.com/?refcode=a15f62e39857" target="_blank" rel="nofollow">DigitalOcean</a>
         </div>
         <div class="hide">
             <script src="http://s11.cnzz.com/stat.php?id=2260758&web_id=2260758"></script>
