00b01977dc725a8fc1968d2ed4ac62e454eb94cb
fiture <fiture.cn@gmail.com>
2014-07-01 21:00:58 +0800

    添加统计代码

--- footer.php
+++ footer.php
@@ -6,6 +6,9 @@
         <div class="grid-990">
         &copy; <?php echo date('Y') ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> (fiture.me)
         </div>
+        <div class="hide">
+            <script src="http://s11.cnzz.com/stat.php?id=2260758&web_id=2260758"></script>
+        </div>
     </footer>
 </body>
 
