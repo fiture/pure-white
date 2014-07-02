af5af85195661721bb902f731ff5f974263d7540
fiture <fiture.cn@gmail.com>
2014-07-02 15:49:42 +0800

    Add IE6 notice

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
         </div>
         <div class="hide">
             <script src="http://s11.cnzz.com/stat.php?id=2260758&web_id=2260758"></script>
+            <!--[if IE 6]><script>alert('\u672C\u7AD9\u5185\u5BB9\u4E0D\u9002\u5408\uFF0C\u4E5F\u4E0D\u6B22\u8FCE\u0049\u0045\u0036\u5185\u6838\u7528\u6237\uFF0C\u8BF7\u7ED5\u9053\uFF0C\u8C22\u8C22\uFF01');location.href = 'about:blank';</script><![endif]-->
         </div>
     </footer>
 </body>
